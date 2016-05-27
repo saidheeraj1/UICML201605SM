ebcad578472eb2640e877422bf88ac3ef5683801
Sam Mourad <Sam.Mourad@crowehorwath.com>
2016-05-26 20:36:32 -0500

    Class updates

--- Class3.R
+++ Class3.R
@@ -8,15 +8,7 @@ rm(list=ls())
 
 #Load Libraries
 library(RODBC)
-
-
-#Set Variables####
-
-#Set environment variables
-dbConnStr = paste0('driver={SQL Server Native Client 11.0};',
-                   'server=.;',
-                   'database=UIC;',
-                   'trusted_connection=yes;')
+source("Class3_Sol.R")
 
 targetVar = 'NextReport'
 
@@ -24,44 +16,11 @@ predThreshold = 0.9
 
 trainPerc = 0.75
 
-#Load and prep Data####
+#Set Variables####
+reportData = loadData(db=T)
 
-#Set up connection and load data object -- replace by a function
-dbConn = odbcDriverConnect(connection=dbConnStr)
-reportData = sqlQuery(dbConn, 'SELECT * FROM dbo.vw_DataFrame')
-close(dbConn)
+dObj = DataSplit(reportData, "NextReport")
 
-
-# #Get the data
-# reportData = loadData()
-
-
-#Number of data points in the training set
-nTrain = ceiling(dim(reportData)[1] * trainPerc)
-
-#Random data points for training set
-idxTrain = sample(x=as.integer(rownames(reportData)), size=nTrain)
-
-#Get Training and Validation sets
-finalData = {}
-finalData$Train = reportData[idxTrain,]
-finalData$Valid = reportData[-idxTrain,]
-
-#Remove index variable
-#Get column index of index variable
-idx = which(colnames(reportData)=="ReportID")
-
-#Move index values to row names and delete index variable
-rownames(finalData$Train) = finalData$Train[,"ReportID"]
-finalData$Train = finalData$Train[, -idx]
-
-rownames(finalData$Valid) = finalData$Valid[,"ReportID"]
-finalData$Valid = finalData$Valid[, -idx]
-
-#Get index of Target Variable
-finalData$targetIdx = which(colnames(finalData$Train)==targetVar)
-
-dObj= finalData
 # #Shuffle/split data for cross-validation
 # dObj = splitData(x=reportData, perc=0.8,
 #                  targetVar=targetVar, indexVar='ReportID')
@@ -85,13 +44,7 @@ rm(list=c('tbt','tbv'))
 
 #Train randomForest
 library(randomForest)
-
-rfMod = randomForest(NextReport ~ .,
-                     data=dObj$Train,
-                     importance=T,
-                     proximity=F,
-                     #mtry=sqrt(dim(dObj$Train)[2]-1),
-                     ntree=100)
+rfMod = trainRF(dObj$Train, dObj$Valid, targetVar, predThreshold)
 
 # #Remove classes with low count
 # tb = as.data.frame(table(reportData$NextReport))
@@ -108,51 +61,14 @@ rfMod = randomForest(NextReport ~ .,
 #                    y=dObj$Train[,tgtIdx],
 #                    ntree=100,
 #                    mtry=6,
-#                    numRandomCuts=1,
+#                    numRandomCuts=10,
 #                    numThreads=1)
 
-
-#Model Evaluation####
-
-#Variable importance
-rfMod$importance
-
-varImpPlot(x=rfMod, main="Variable Importance")
-
-rfMod$ntree
-rfMod$mtry
-
-#Cross-Validation
-predMat = predict(object=rfMod,
-                  newdata=dObj$Valid[,-dObj$targetIdx],
-                  type='prob')
-
-View(predMat)
-
-predMax = apply(X=predMat, MARGIN=1, FUN=max)
-predCol = max.col(m=predMat, ties.method='first')
-predLab = colnames(predMat)[predCol]
-
-predObj = {}
-predObj$Prob = predMax
-predObj$Label = predLab
-predObj$Pred = predObj$Label
-predObj$Pred[predObj$Prob<0.9] = 'N/A'
-predObj$Actual = as.character(dObj$Valid$NextReport)
-predObj$predPerc = sum(predObj$Pred!='N/A')/length(predObj$Label)
-predObj$errorRate = 
-  sum(predObj$Pred!=predObj$Actual & predObj$Pred!='N/A')/
-    sum(predObj$Pred!='N/A')
-
 sprintf('Rate of Prediction: %f, Error rate: %f',
-        predObj$predPerc, predObj$errorRate)
-
-View(predObj)
-
-rm(list= c('predMax', 'predCol', 'predLab'))
+        rfMod$predPerc, predObj$errorRate)
 
 #Graph Error Rate and Rate of Prediction against Prediction Threshold
-threshmat = matrix(NaN, nrow=11, ncol=3)
+threshmat = matrix(NaN, nrow=101, ncol=3)
 colnames(threshmat) = c('Thresh', 'Error', 'Pred')
 threshstep = 1/(dim(threshmat)[1] - 1)
 
@@ -165,10 +81,10 @@ for (i in 1:dim(threshmat)[1]){
   threshmat[i,3] = npred / ntot
 }
 
-
+x11(8,6)
 #p = recordPlot()
 par(mar=c(5,4,4,6) + 0.1)
-plot(x=threshmat[,1], y=threshmat[,2], type='b',
+plot(x=threshmat[,1], y=threshmat[,2], type='l',
      main='Prediction Threshold',
      col='#FF4500', axes=FALSE, xlab="Threshold", ylab="", pch=1)
 axis(side=1, at=threshmat[,1][
@@ -177,7 +93,7 @@ axis(side=2, ylim=c(0,max(threshmat[,2])), col=c('#FF4500','#FF4500'),
      col.axis='#FF4500')
 mtext(side=2, text='Error Rate', col='#FF4500', line=2.5)
 par(new=TRUE)
-plot(x=threshmat[,1], y=threshmat[,3], type='b',
+plot(x=threshmat[,1], y=threshmat[,3], type='l',
      col='#008000', axes=FALSE, xlab="", ylab="", pch=2)
 axis(side=4, ylim=c(0,max(threshmat,3)), col=c('#008000','#008000'),
      col.axis='#008000')
--- Class3_Sol.R
+++ Class3_Sol.R
@@ -1,8 +1,4 @@
-targetVar = 'NextReport'
 
-predThreshold = 0.9
-
-trainPerc = 0.75
 
 DataSplit = function(x, targetVar, removeFirst = T, perc = .75){
 
@@ -34,18 +30,62 @@ DataSplit = function(x, targetVar, removeFirst = T, perc = .75){
   return(finalData)
 }
 
-loadData = function(){
-  
-  #Set environment variables
-  dbConnStr = paste0('driver={SQL Server Native Client 11.0};',
-                     'server=.;',
-                     'database=UIC2;',
-                     'trusted_connection=yes;')
-  
-  #Set up connection and load data object -- replace by a function
-  dbConn = odbcDriverConnect(connection=dbConnStr)
-  reportData = sqlQuery(dbConn, 'SELECT * FROM dbo.vw_DataFrame')
-  close(dbConn)
+loadData = function(db=T){
+  if(db){
+    #Set environment variables
+    dbConnStr = paste0('driver={SQL Server Native Client 11.0};',
+                       'server=.;',
+                       'database=UIC2;',
+                       'trusted_connection=yes;')
+    
+    #Set up connection and load data object -- replace by a function
+    dbConn = odbcDriverConnect(connection=dbConnStr)
+    reportData = sqlQuery(dbConn, 'SELECT * FROM dbo.vw_DataFrame')
+    close(dbConn)
+  }else{
+    reportData = read.csv("report.csv")
+    names(reportData)[1] = 'ReportID'
+  }
   
   return(reportData)
 }
+
+
+trainRF = function(x.train, x.valid, targetVar, threshold=0.9, ...){
+  trainFormula = formula(paste0(targetVar, " ~ ."))
+  rfM = randomForest(formula=trainFormula,
+                     data=x.train, ...)
+  
+  #Cross-Validation
+  targetIdx = which(names(x.valid)==targetVar)
+  predMat = predict(object=rfM,
+                    newdata=x.valid[,-targetIdx],
+                    type='prob')
+  
+  predMax = apply(X=predMat, MARGIN=1, FUN=max)
+  predCol = max.col(m=predMat, ties.method='first')
+  predLab = colnames(predMat)[predCol]
+  
+  validObj = {}
+  validObj$Prob = predMax
+  validObj$Label = predLab
+  validObj$Pred = validObj$Label
+  validObj$Pred[validObj$Prob<threshold] = 'N/A'
+  validObj$Actual = as.character(x.valid[,targetVar])
+  validObj$predPerc = sum(validObj$Pred!='N/A')/length(validObj$Label)
+  validObj$errorRate = 
+    sum(validObj$Pred!=validObj$Actual & validObj$Pred!='N/A')/
+    sum(validObj$Pred!='N/A')
+  
+  sprintf('Rate of Prediction: %f, Error rate: %f',
+          validObj$predPerc, validObj$errorRate)
+  
+  rfMod = {}
+  rfMod$mod = rfM
+  rfMod$valid = validObj[1:4]
+  rfMod$errorRate = validObj$errorRate
+  rfMod$predPerc = validObj$predPerc
+  
+  return(rfMod)
+}
+
